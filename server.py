ba2d8c32d68fbe66ba9b5fa2bff670122d12a93f
Erwin Kooi <github@cdyweb.com>
2018-10-12 08:52:59 -0400

    pyGitHub (#3)
    * ignore pycharm IDE files
    
    * remove unneeded ../ from templates
    
    * implement PyGithub for API calls

--- server.py
+++ server.py
@@ -1,37 +1,26 @@
-
 from flask import Flask, render_template, request, url_for, redirect
-import requests
-import json
-
+from github import Github, UnknownObjectException
+from requests.exceptions import ConnectionError
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/profile', methods = ['POST', 'GET'])
 
+@app.route('/profile', methods = ['POST', 'GET'])
 def profile():
     if request.method == 'POST':
         username = request.form['username']
         try:
-            req = requests.get('https://api.github.com/users/' + username)
-            if req.ok:
-                github_data = json.loads(req.content)
-                name = github_data['name']
-                profile_pic = github_data['avatar_url']
-                repos = github_data['public_repos']
-                followers = github_data['followers']
-                following = github_data['following']
-                gists = github_data['public_gists']
-                return render_template('profile.html', username = username, name = name, src = profile_pic, repos = repos, followers = followers, following = following, gists = gists)
-            else:
-                return render_template('profile.html', error = True)
-        except:
+            user = Github().get_user(username)
+            return render_template('profile.html', user=user)
+        except UnknownObjectException:
+            return render_template('profile.html', error=True)
+        except ConnectionError:
             return render_template('connectionError.html')
 
 
 if __name__ == '__main__':
-    #print("Navigate to localhost:5000")
-    app.run(debug = True)
+    app.run(debug=True)
